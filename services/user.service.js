e2f8c845e3a8b077eeda61ca517f4954b1f8cbe6
castillomiguel25 <miguelangelcastillo947@gmail.com>
2023-05-29 22:03:16 -0400

    'presentation'

---
+++ services/user.service.js
@@ -0,0 +1,33 @@
+const boom = require('@hapi/boom');
+const getConection = require('../libs/postgres')
+
+class UserService {
+  constructor() {}
+
+  async create(data) {
+    return data;
+  }
+
+  async find() {
+    const client = await getConection();
+    const res = await client.query('SELECT * FROM tasks');
+    return res.rows
+  }
+
+  async findOne(id) {
+    return { id };
+  }
+
+  async update(id, changes) {
+    return {
+      id,
+      changes,
+    };
+  }
+
+  async delete(id) {
+    return { id };
+  }
+}
+
+module.exports = UserService;
